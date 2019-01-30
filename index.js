38b476b35d40fb7c3f8e832fc90edf5d957b5692
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-30 03:12:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function currentLine (katzDeliLine){
   }
 
   var myString = `The line is currently: 1. ${katzDeliLine[0]}`
-
+for(let i = 1; i<katzDeliLine.length;i++){
+  myString += `, ${i+1}. ${katzDeliLine[i]}`
+}
+return myString
+}
   
