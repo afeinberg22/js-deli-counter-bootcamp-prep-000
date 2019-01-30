e13b7b71aa5ac549e1ccbc0bc09fe36bd3f5119f
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-30 03:07:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function takeANumber (katzDeliLine, name){
+  var position = katzDeliLine.length+1
+  katzDeliLine.push(name)
+return "Welcome, " + name +". You are number "+ position +" in line."
+}
